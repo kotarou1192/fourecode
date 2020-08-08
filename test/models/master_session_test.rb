a801915c2e2b657e02fb6b934842f378427ae047
kousuke_ida <takashiii1192@gmail.com>
2020-08-08 17:38:13 +0900

    add test for master ssession

--- test/models/master_session_test.rb
+++ test/models/master_session_test.rb
@@ -14,10 +14,13 @@ class MasterSessionTest < ActiveSupport::TestCase
                      password: 'hogefuga')
     @user.save
     @master_session = @user.master_session.create
+    @onetime_session = @master_session.onetime_session.new
+    @onetime_session.user = @user
+    @onetime_session.save!
   end
 
   test 'should be present' do
-    assert @master_session
+    assert @master_session && @onetime_session
   end
 
   test 'the token should be able to refer' do
@@ -39,5 +42,13 @@ class MasterSessionTest < ActiveSupport::TestCase
     @master_session.update(created_at: 100.days.ago)
 
     assert_not @master_session.available?
+  end
+
+  test 'onetime session must not exist if master session deleted' do
+    @master_session.destroy!
+
+    onetime_session = OnetimeSession.find_by(user_id: @user.id)
+
+    assert_not onetime_session
   end
 end
