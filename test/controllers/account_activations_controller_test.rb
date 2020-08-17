2f3e0440f8ccde2f28293bbac2b7755c4f2a95aa
kousuke_ida <takashiii1192@gmail.com>
2020-08-17 20:02:06 +0900

    add: activation tests

--- test/controllers/account_activations_controller_test.rb
+++ test/controllers/account_activations_controller_test.rb
@@ -4,4 +4,24 @@ class AccountActivationsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+  def setup
+    @email = 'hoge@email.com'
+    @user = User.new(name: 'hoge',
+                     nickname: 'hogechan',
+                     email: @email,
+                     password: 'hogefuga')
+    @user.save
+  end
+
+  test 'invalid activate link responses status 400' do
+    put '/api/v1/account_activations', params: { hoge: { token: 'hogefuga', email: 'hogefuga@fuga.fuga' } }
+    body = JSON.parse(response.body)
+    assert response.status == 400 && body['errors'][0]['key'] == 'link'
+  end
+
+  test 'the user should be activated' do
+    put '/api/v1/account_activations', params: { value: { token: @user.activation_token, email: @email } }
+
+    assert response.status == 200
+  end
 end
