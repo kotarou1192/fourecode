0154ff7303e8970e9d4d7dfd7499fb6eb4bb8e6c
kousuke_ida <takashiii1192@gmail.com>
2020-08-17 19:54:26 +0900

    update: use strong parameters

--- app/controllers/api/v1/account_activations_controller.rb
+++ app/controllers/api/v1/account_activations_controller.rb
@@ -4,15 +4,25 @@ class Api::V1::AccountActivationsController < ApplicationController
   include ErrorMessageHelper
 
   def update
-    user = User.find_by(email: params[:value][:email])
+    user = User.find_by(email: user_params[:email])
 
-    if user && !user.activated? && user.authenticated?(:activation, params[:value][:token])
+    if user && !user.activated? && user.authenticated?(:activation, user_params[:token])
       user.activate
       render json: { status: 'SUCCESS', body: { message: 'activated' } }
     else
       message = 'invalid activation link'
       render status: 400, json: { status: 'ERROR', message: message }
-        .merge(error_messages(key: 'link', message: message))
+                                  .merge(error_messages(key: 'link', message: message))
     end
+
+
+  end
+
+  private
+
+  def user_params
+    return {} unless params[:value]
+
+    params.require(:value).permit(:email, :token)
   end
 end
