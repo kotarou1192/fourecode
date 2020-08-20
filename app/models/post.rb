29ba3dcaed23e89589b33f6fad2ac97f2979fa78
kousuke_ida <takashiii1192@gmail.com>
2020-08-20 16:01:03 +0900

    cosme: replace defined_status

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   DEFAULT_REWARD = 100
+  DEFINED_STATES = %w[accepting voting resolved].freeze
 
   before_save :set_default_reward
   validates :body, presence: true
@@ -8,8 +9,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   def change_state(state)
-    defined_states = %w[accepting voting resolved]
-    unless defined_states.any?(state)
+    unless DEFINED_STATES.any?(state)
       raise ArgumentError, 'the state does not match any defined types'
     end
 
