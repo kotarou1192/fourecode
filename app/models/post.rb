3cfbe4a3395b493a1e45830b77d60b76ec76299e
kousuke_ida <takashiii1192@gmail.com>
2020-08-20 15:18:30 +0900

    update: implemented a model of the post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,32 @@
 class Post < ApplicationRecord
+  DEFAULT_REWARD = 100
+
+  before_save :set_default_reward
+  validates :body, presence: true
+  validates :code, presence: true
+
+  belongs_to :user
+
+  def change_state(state)
+    defined_states = %w[accepting voting resolved]
+    unless defined_states.any?(state)
+      raise ArgumentError, 'the state does not match any defined types'
+    end
+
+    update_attribute(:state, state)
+  end
+
+  def resolve
+    change_state('resolved')
+  end
+
+  def resolved?
+    state == 'resolved'
+  end
+
+  private
+
+  def set_default_reward
+    self.bestanswer_reward ||= DEFAULT_REWARD
+  end
 end
