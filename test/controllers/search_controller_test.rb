dd25a38e562b877f3efcac9ae066f50bf671b7ad
kousuke_ida <takashiii1192@gmail.com>
2020-08-28 16:12:19 +0900

    add: tests for search users api

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -24,6 +24,7 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
     [master_session, onetime_session]
   end
 
+  # search posts test
   test 'should be found' do
     get '/api/v1/search/posts', params: { keyword: 't' }
     body = JSON.parse(response.body)
@@ -39,6 +40,25 @@ class SearchControllerTest < ActionDispatch::IntegrationTest
 
     body['errors'].each do |error|
       assert error['key'] == 'keyword'
+    end
+  end
+
+  # search users test
+  test 'too many keywords should be rejected when search users' do
+    get '/api/v1/search/users', params: { keyword: 't e s t k e y w o r d s i s t o o m a n y' }
+    body = JSON.parse(response.body)
+
+    body['errors'].each do |error|
+      assert error['key'] == 'keyword'
+    end
+  end
+
+  test 'users should be found' do
+    get '/api/v1/search/users', params: { keyword: 'hoge' }
+    body = JSON.parse(response.body)
+
+    body['body']['results'].each do |result|
+      assert result['name'] == @user.name
     end
   end
 end
