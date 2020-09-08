c21cfc10344f073755e56b5c5636a3f952f379b9
kousuke_ida <takashiii1192@gmail.com>
2020-09-08 12:03:47 +0900

    add: make possible to review a post from a web api

---
+++ app/controllers/api/v1/reviews_controller.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+class Api::V1::ReviewsController < ApplicationController
+  include UserHelper
+
+  before_action :get_user, only: %i[create]
+  # before_action :get_session_owner, only: %i[show]
+
+  def create
+    return unless @user
+
+    post = Post.find(post_id)
+    unless post
+      message = 'the post is not found'
+      return error_response json: generate_response(FAILED, message)
+                                    .merge(error_messages(key: 'post_id', message: message))
+    end
+
+    if post.closed?
+      message = 'the post has been closed'
+      return error_response json: generate_response(FAILED, message)
+                                    .merge(error_messages(key: 'closed', message: message))
+    end
+
+    review = Review.generate_record(body: body, post: post, user: @user)
+    if review.save
+      return render json: generate_response(SUCCESS, 'the review has been created successfully')
+    end
+
+    failed_to_create review
+  end
+
+  def show
+    body = ShowReview.show(post_id)
+    render json: generate_response(SUCCESS, body)
+  end
+
+  private
+
+  def failed_to_create(response)
+    messages = generate_error_messages_from_errors(response.errors.messages)
+    error_response json: generate_response(FAILED, nil)
+                           .merge(error_messages(error_messages: messages))
+  end
+
+  def post_id
+    return nil unless params[:id]
+
+    params.permit(:id)[:id]
+  end
+
+  def body
+    return nil unless params[:value] && params[:value][:body]
+
+    params.require(:value).permit(:body)[:body]
+  end
+end
